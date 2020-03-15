fdc071bf6cddf6c864ff21760b1d1ff3538e2855
VOIDMalkuth <you@example.com>
2020-03-15 23:24:03 +0000

    Finished lab1-extra

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -6,6 +6,27 @@
 
 #include "kerelf.h"
 #include <stdio.h>
+
+#define EI_DATA (5)
+
+uint32_t rev32Word(uint32_t word) {
+    uint32_t revWord = 0;
+    revWord = word >> 24;
+    revWord |= (word >> 8) & 0x0000FF00;
+    revWord |= (word << 8) & 0x00FF0000;
+    revWord |= (word << 24) & 0xFF000000;
+
+    return revWord;
+}
+
+uint16_t rev16Word(uint16_t half) {
+    uint16_t revHalf = 0;
+    revHalf = half >> 8;
+    revHalf |= (half << 8) & 0xFF00;
+
+    return revHalf;
+}
+
 /* Overview:
  *   Check whether it is a ELF file.
  *
@@ -49,28 +70,55 @@ int readelf(u_char *binary, int size)
 
         int Nr;
 
+        int isBigEndian = 0;
+
         Elf32_Shdr *shdr = NULL;
+        Elf32_Phdr *phdr = NULL;
 
         u_char *ptr_sh_table = NULL;
         Elf32_Half sh_entry_count;
         Elf32_Half sh_entry_size;
 
-
+        u_char *ptr_ph_table = NULL;
+        Elf32_Half ph_entry_count;
+        Elf32_Half ph_entry_size;
         // check whether `binary` is a ELF file.
         if (size < 4 || !is_elf_format(binary)) {
                 printf("not a standard elf format\n");
                 return 0;
         }
+        // check if is Big Endian
+        isBigEndian = ehdr->e_ident[EI_DATA] == 2;
 
         // get section table addr, section header number and section header size.
-		shdr = (Elf32_Shdr *)(ehdr->e_shoff + (u_char *)binary);
-		sh_entry_count = ehdr->e_shnum;
-		sh_entry_size = ehdr->e_shentsize;
+		int real_shoff = isBigEndian ? rev32Word(ehdr->e_shoff) : ehdr->e_shoff;
+        shdr = (Elf32_Shdr *)(real_shoff + (u_char *)binary);
+		sh_entry_count = isBigEndian ? rev16Word(ehdr->e_shnum) : ehdr->e_shnum;
+		sh_entry_size = isBigEndian ? rev16Word(ehdr->e_shentsize) : ehdr->e_shentsize;
+
+        // get program table addr, program header number and program header size.
+		int real_phoff = isBigEndian ? rev32Word(ehdr->e_phoff) : ehdr->e_phoff;
+        phdr = (Elf32_Phdr *)(real_phoff + (u_char *)binary);
+		ph_entry_count = isBigEndian ? rev16Word(ehdr->e_phnum) : ehdr->e_phnum;
+		ph_entry_size = isBigEndian ? rev16Word(ehdr->e_phentsize) : ehdr->e_phentsize;
+
         // for each section header, output section number and section addr.
-		int i = 0;
+		/*
+        int i = 0;
 		for (i = 0; i < sh_entry_count; i++) {
-			printf("%d:0x%x\n", i, shdr[i].sh_addr);
+            uint32_t real_sh_addr = isBigEndian ? rev32Word(shdr[i].sh_addr) : shdr[i].sh_addr;
+			printf("%d:0x%x\n", i, real_sh_addr);
 		}
+        */
+
+        int i = 0;
+		for (i = 0; i < ph_entry_count; i++) {
+            uint32_t real_ph_filesz = isBigEndian ? rev32Word(phdr[i].p_filesz) : phdr[i].p_filesz;
+            uint32_t real_ph_memsz = isBigEndian ? rev32Word(phdr[i].p_memsz) : phdr[i].p_memsz;
+
+			printf("%d:0x%x,0x%x\n", i, real_ph_filesz, real_ph_memsz);
+		}
+
         // hint: section number starts at 0.
 
 
