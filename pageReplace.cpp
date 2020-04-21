cc830f18a22ff787b00d66cc06f6819d7a0953fe
VOIDMalkuth <you@example.com>
2020-04-21 16:45:07 +0000

    fifo64

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -1,13 +1,12 @@
 // pageReplace.cpp
-#include "pageReplace.h" // 测评需求，请务必包含该头文件
+//#include "pageReplace.h" // 测评需求，请务必包含该头文件
 #define MAX_PHY_PAGE 64
 #define MAX_PAGE 12
 #define get_Page(x) (x >> MAX_PAGE)
 
 int find(long *array, int n, long val);
 
-inline int find(long *array, int n, long val)
-{
+inline int find(long *array, int n, long val) {
     for (int i = 0; i < n; i++)
     {
         if (array[i] == val)
@@ -20,30 +19,15 @@ inline int find(long *array, int n, long val)
 
 int helpArray[MAX_PHY_PAGE + 1];
 
-void pageReplace(long *physic_memery, long nwAdd)
-{
-    static int cursor = 1;
+void pageReplace(long *physic_memery, long nwAdd) {
+    static int head = 0;
     int pg = get_Page(nwAdd);
-    int frameVisited = find(physic_memery, MAX_PHY_PAGE, pg);
-    if (frameVisited != -1)
-    {
-        helpArray[frameVisited] = cursor;
-        cursor++;
+
+    if (find(physic_memery, MAX_PHY_PAGE, pg) != -1) {
         return;
     }
-    // find min in helpArray
-    int swapIndex = 0;
-    int swapDistance = helpArray[0];
-    for (int i = 0; i < MAX_PHY_PAGE; i++)
-    {
-        if (helpArray[i] < swapDistance)
-        {
-            swapIndex = i;
-            swapDistance = helpArray[i];
-        }
-    }
-    helpArray[swapIndex] = cursor++;
-    physic_memery[swapIndex] = pg;
+    physic_memery[head] = pg;
+    head = (head + 1) % MAX_PAGE;
 
     return;
 }
