7b09c6c3beb39ef47050ed09f918974e0eacb5ce
VOIDMalkuth <you@example.com>
2020-05-05 18:48:23 +0000

    Lab4-Extra Advanced Part-2 - broken version

---
+++ user/pp_a_c.c
@@ -0,0 +1,25 @@
+#include "lib.h"
+
+#define AENVID (0x800)
+#define BENVID (0x1001)
+#define CENVID (0x1802)
+
+void
+umain(void)
+{
+    int r;
+    int num = 0;
+    u_int sender = 0;
+    
+    // start
+    while (num < 5) {
+        writef("%x am waiting.....\n", syscall_getenvid());
+        num = ipc_recv(&sender, 0, 0);
+        writef("%x got %d from %x\n", syscall_getenvid(), num, sender);
+        num = num + 1;
+        writef("\n@@@@@send %d from %x to %x\n", syscall_getenvid(), num, BENVID);
+        ipc_send(BENVID, num, 0, 0);
+    }
+
+    return;
+}
---
+++ user/pp_b.c
@@ -0,0 +1,42 @@
+#include "lib.h"
+
+#define AENVID (0x800)
+#define BENVID (0x1001)
+#define CENVID (0x1802)
+
+void
+umain(void)
+{
+    int r;
+    int aNum = -1;
+    int bNum = -1;
+    u_int sender = 0;
+    int tmpNum = 0;
+    
+    // start
+    while (aNum < 5 || bNum < 5) {
+        writef("\n@@@@@send %d from %x to %x and %x\n", aNum + 1, syscall_getenvid(), AENVID, BENVID);
+        r = ipc_send_double(0x800, 0x1802, aNum + 1, 0, 0);
+        writef("%x am waiting.....\n", syscall_getenvid());
+        tmpNum = ipc_recv(&sender, 0, 0);
+        writef("%x got %d from %x\n", syscall_getenvid(), tmpNum, sender);
+        if (sender == AENVID) {
+            aNum = tmpNum;
+        } else {
+            bNum = tmpNum;
+        }
+
+        writef("%x am waiting.....\n", syscall_getenvid());
+        tmpNum = ipc_recv(&sender, 0, 0);
+        writef("%x got %d from %x\n", syscall_getenvid(), tmpNum, sender);
+        if (sender == AENVID) {
+            aNum = tmpNum;
+        } else {
+            bNum = tmpNum;
+        }
+
+        user_assert(aNum == bNum);
+    }
+
+    return;
+}
