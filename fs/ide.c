21f4e7e114063e808177b5784a27be469bbf3a15
VOIDMalkuth <you@example.com>
2020-05-11 18:16:28 +0000

    Lab5 Exercise 5.2

--- fs/ide.c
+++ fs/ide.c
@@ -6,6 +6,30 @@
 #include "lib.h"
 #include <mmu.h>
 
+int ideOperation(int op, u_int diskno, u_int offset) {
+	u_int ideMemBase = 0xA0000000 + 0x13000000;
+	volatile u_char *ideOpAddr = (volatile u_char *)(ideMemBase + 0x0020);
+	volatile u_int *ideDiskAddr = (volatile u_int *)(ideMemBase + 0x0010);
+	volatile u_int *ideOffsetAddr = (volatile u_int *)(ideMemBase + 0x0000);
+	volatile u_int *ideResultAddr = (volatile u_int *)(ideMemBase + 0x0030);
+	const int ideOp_read = 0;
+	const int ideOp_write = 1;
+
+	if (op != ideOp_read && op != ide_write) {
+		user_panic("IDE Operation %d illegal!\n", op);
+	}
+	
+	if ((offset & (BY2SECT - 1)) != 0) {
+		user_panic("IDE Offset 0x%x is not aligned!\n", offset);
+	}
+
+	*ideDiskAddr = diskno;
+	*ideOffsetAddr = offset;
+	*ideOpAddr = op;
+
+	return *ideResultAddr;
+}
+
 // Overview:
 // 	read data from IDE disk. First issue a read request through
 // 	disk register and then copy data from disk buffer
@@ -24,15 +48,28 @@
 void
 ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
 {
-	// 0x200: the size of a sector: 512 bytes.
+    // 0x200: the size of a sector: 512 bytes.
 	int offset_begin = secno * 0x200;
 	int offset_end = offset_begin + nsecs * 0x200;
 	int offset = 0;
 
+	u_int ideDataBase = 0xA0000000 + 0x13000000 + 0x4000;
+	const int ideOp_read = 0;
+
 	while (offset_begin + offset < offset_end) {
-            // Your code here
-            // error occurred, then panic.
+		int r = ideOperation(ideOp_read, diskno, offset_begin + offset);
+		// 0 means failure in ide!
+		if (r == 0) {
+			user_panic("IDE operation failed, op: %d, diskno: %d, offset: 0x%x\n", ideOp_read, diskno, offset_begin + offset);
+		}
+		r = syscall_read_dev(dst + offset, ideDataBase, BY2SECT);
+		if (r != 0) {
+			user_panic("Read_dev(IDE data) failed in IDE, va: 0x%x\n", dst + offset);
+		}
+		offset += BY2SECT;
 	}
+
+	return;
 }
 
 
@@ -52,15 +89,27 @@ ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
 void
 ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
 {
-        // Your code here
-	// int offset_begin = ;
-	// int offset_end = ;
-	// int offset = ;
-	writef("diskno: %d\n", diskno);
-	// while ( < ) {
-	    // copy data from source array to disk buffer.
+    int offset_begin = secno * BY2SECT;
+	int offset_end = offset_begin + nsecs * BY2SECT;
+	int offset = 0;
+	
+	u_int ideDataBase = 0xA0000000 + 0x13000000 + 0x4000;
+	const int ideOp_write = 1;
 
-            // if error occur, then panic.
-	// }
+	writef("diskno: %d\n", diskno);
+	while (offset_begin + offset < offset_end) {
+	    int r = syscall_write_dev(src + offset, ideDataBase, BY2SECT);
+		if (r != 0) {
+			user_panic("Write_dev(IDE data) failed in IDE, va: 0x%x\n", src + offset);
+		}
+		r = ideOperation(ideOp_write, diskno, offset_begin + offset);
+		// 0 means failure in ide!
+		if (r == 0) {
+			user_panic("IDE operation failed, op: %d, diskno: %d, offset: 0x%x\n", ideOp_write, diskno, offset_begin + offset);
+		}
+		offset += BY2SECT;
+    }
+
+	return;
 }
 
