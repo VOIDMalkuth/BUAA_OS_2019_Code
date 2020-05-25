873c179434d0c0ac0d56977f8304fb99a59d1f28
VOIDMalkuth <you@example.com>
2020-05-25 12:32:42 +0000

    Lab-6: Exercise 6.3

--- init/init.c
+++ init/init.c
@@ -23,11 +23,11 @@ void mips_init()
 	//ENV_CREATE(user_pt1);
 	//ENV_CREATE(user_idle);
 	//ENV_CREATE(user_fktest);
-	//ENV_CREATE(user_pingpong);
+	ENV_CREATE(user_pingpong);
 	//ENV_CREATE(user_testfdsharing);	
 	ENV_CREATE(user_testpipe);
-	//ENV_CREATE(user_testpiperace);
-	ENV_CREATE(fs_serv);
+	ENV_CREATE(user_testpiperace);
+	//ENV_CREATE(fs_serv);
 
 	//ENV_CREATE(user_icode);
 	trap_init();
