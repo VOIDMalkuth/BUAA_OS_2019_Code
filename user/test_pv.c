1df572e5036387093baa49043f050f1cf7f945cb
VOIDMalkuth <you@example.com>
2020-06-01 18:26:07 +0000

    Lab-6 Extra: test - more obvious and release

--- user/test_pv.c
+++ user/test_pv.c
@@ -3,22 +3,37 @@
 void umain() {
     u_int sem_id = syscall_init_PV_var(0);
     u_int childId = fork();
-    writef("Semid: %d\n", sem_id);
     if (childId == 0) {
+        syscall_yield();
         int i = 0;
-        while (i < 30) {
-            writef("Semval [%d]\n", syscall_check_PV_value(sem_id));
-            i++;
-            writef("Prdc %d\n", i);
+        while (i < 19) {
             syscall_V(sem_id);
+            writef("S-val <%d>\n", syscall_check_PV_value(sem_id));
+            i++;
+            writef("[%x]Prdc %d\n", env->env_id, i);
         }
+        i = 0;
+        while (i < 2000) {
+            syscall_yield();
+            i++;
+        }
+        writef("Env [%x] produced 20, releasing and terminating others...\n");
+        syscall_release_PV_var(sem_id);
+        user_panic("Should Stop all");
     } else {
+        fork();
+        fork();
         int j = 0;
-        while (j < 30) {
+        while (j < 20) {
+            writef("Try to p\n");
             syscall_P(sem_id);
-            writef("Semval [%d]\n", syscall_check_PV_value(sem_id));
+            int val = syscall_check_PV_value(sem_id);
+            if (val == -3) {
+                user_panic("1111");
+            }
+            writef("S-val <%d>\n", syscall_check_PV_value(sem_id));
             j++;
-            writef("Cnsm %d\n", j);
+            writef("[%x]Cnsm %d\n", env->env_id, j);
         }
     }
 }
