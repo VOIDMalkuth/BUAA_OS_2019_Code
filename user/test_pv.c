abd64a64aafbb5007dcb9f54c0da5c8bd66e94b0
VOIDMalkuth <you@example.com>
2020-06-01 17:33:01 +0000

    Lab-6 Extra: test - more obvious

--- user/test_pv.c
+++ user/test_pv.c
@@ -7,18 +7,18 @@ void umain() {
     if (childId == 0) {
         int i = 0;
         while (i < 30) {
-            syscall_V(sem_id);
-            writef("Sem val is %d\n", syscall_check_PV_value(sem_id));
+            writef("Semval [%d]\n", syscall_check_PV_value(sem_id));
             i++;
-            writef("Producing %d\n", i);
+            writef("Prdc %d\n", i);
+            syscall_V(sem_id);
         }
     } else {
         int j = 0;
         while (j < 30) {
             syscall_P(sem_id);
-            writef("Sem val is %d\n", syscall_check_PV_value(sem_id));
+            writef("Semval [%d]\n", syscall_check_PV_value(sem_id));
             j++;
-            writef("Consuming %d\n", j);
+            writef("Cnsm %d\n", j);
         }
     }
 }
