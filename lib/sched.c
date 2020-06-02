38141cbd752256d8d17311ab3922abfd16b26f71
VOIDMalkuth <you@example.com>
2020-06-02 04:43:03 +0000

    Revert "Lab-6 Extra: Try sched again and again"
    This reverts commit e967798231a24ea5167ec4898994d795b161550e.

--- lib/sched.c
+++ lib/sched.c
@@ -2,100 +2,79 @@
 #include <pmap.h>
 #include <printf.h>
 
-/* Overview:
- *  Implement simple round-robin scheduling.
- *  Search through 'envs' for a runnable environment ,
- *  in circular fashion statrting after the previously running env,
- *  and switch to the first such environment found.
- *
- * Hints:
- *  The variable which is for counting should be defined as 'static'.
- */
-
-extern struct Env_list env_sched_list[];
-extern struct Env *curenv;
-
 int syscall_sched_forceReSchedule = 0;
 
+/* Overview:
+ *  Implement simple round-robin scheduling.
+ *
+ *
+ * Hints:
+ *  1. The variable which is for counting should be defined as 'static'.
+ *  2. Use variable 'env_sched_list', which is a pointer array.
+ *  3. CANNOT use `return` statement!
+ */
+/*** exercise 3.14 ***/
 void sched_yield(void)
 {
-	/*
-	static u_int cur_lasttime = 1;
-	struct Env *next_env;
-	cur_lasttime--;
-	if (cur_lasttime == 0 || curenv == NULL)
-	{
-		if (curenv != NULL)
-		{
-			LIST_INSERT_TAIL(&env_sched_list[1], curenv, env_sched_link);
-		}
-		if (!LIST_EMPTY(&env_sched_list[0]))
-		{
-			next_env = LIST_FIRST(&env_sched_list[0]);
-			LIST_REMOVE(next_env, env_sched_link);
-		}
-		else
-		{
-			next_env = LIST_FIRST(&env_sched_list[1]);
-			LIST_REMOVE(next_env, env_sched_link);
-		}
-		cur_lasttime = next_env->env_pri;
-		//printf("%x %x\n",curenv, next_env);
-		env_run(next_env);
-	}
-	env_run(curenv);
-	//printf("%x",*((int*)(TF_EPC + TIMESTACK - TF_SIZE)));
-	//printf("Sched_yield End\n");
-	*/
-	//printf("Sched_yield happen\n");
-	static int cur_lasttime = 1;
-	static int cur_head_index = 0;
-	struct Env *next_env;
-	int now_have = 0;
-	cur_lasttime--;
-	if (cur_lasttime <= 0 || curenv == NULL || curenv->env_status != ENV_RUNNABLE)
-	{
-		now_have = 0;
-		while(1)
-		{
-			if (LIST_EMPTY(&env_sched_list[cur_head_index]))
-			{
-				cur_head_index = !cur_head_index;
-				break;
-			}
-			next_env = LIST_FIRST(&env_sched_list[cur_head_index]);
-			if (next_env->env_status == ENV_RUNNABLE)
-			{
-				now_have = 1;
-				break;
-			}
-			LIST_REMOVE(next_env, env_sched_link);
-			LIST_INSERT_HEAD(&env_sched_list[!cur_head_index], next_env, env_sched_link);
-		}
-		if (!now_have)
-		{
-			while (1)
-			{
-				if (LIST_EMPTY(&env_sched_list[cur_head_index]))
-				{
-					panic("^^^^^^No env is RUNNABLE!^^^^^^");
-				}
-				next_env = LIST_FIRST(&env_sched_list[cur_head_index]);
-				if (next_env->env_status == ENV_RUNNABLE)
-				{
-					now_have = 1;
-					break;
-				}
-				LIST_REMOVE(next_env, env_sched_link);
-				LIST_INSERT_HEAD(&env_sched_list[!cur_head_index], next_env, env_sched_link);
-			}
-		}
-		LIST_REMOVE(next_env, env_sched_link);
-		LIST_INSERT_HEAD(&env_sched_list[!cur_head_index], next_env, env_sched_link);
-		cur_lasttime = next_env->env_pri;
-		env_run(next_env);
-		panic("^^^^^^sched yield jump faild^^^^^^");
-	}
-	env_run(curenv);
-	panic("^^^^^^sched yield reached end^^^^^^");
+    static int count = 0; // remaining time slices of current env
+    static int point = 0; // current env_sched_list index
+    
+    /*  hint:
+     *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
+     *     using LIST_REMOVE and LIST_INSERT_TAIL.
+     *  2. if (env_sched_list[point] is empty), point = 1 - point;
+     *     then search through `env_sched_list[point]` for a runnable env `e`, 
+     *     and set count = e->env_pri
+     *  3. count--
+     *  4. env_run()
+     *
+     *  functions or macros below may be used (not all):
+     *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
+     */
+
+    if (syscall_sched_forceReSchedule) {
+        count = 0;
+        syscall_sched_forceReSchedule = 0;
+    }
+    
+    struct Env *e = curenv;
+    if (count != 0 && e != NULL && e->env_status == ENV_RUNNABLE) {
+        count--;
+        env_run(e);
+    } else if (e != NULL) {
+        LIST_REMOVE(e, env_sched_link);
+        LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
+    }
+
+    // reschedule
+    e = LIST_FIRST(&env_sched_list[point]);
+
+    if (LIST_EMPTY(&env_sched_list[point])) {
+        point = 1 - point;
+    }
+    
+    do {
+        e = LIST_FIRST(&env_sched_list[point]);
+        if (e != NULL && e->env_status == ENV_NOT_RUNNABLE) {
+            LIST_REMOVE(e, env_sched_link);
+            LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
+        } else if (e != NULL && e->env_status == ENV_FREE) {
+            LIST_REMOVE(e, env_sched_link);
+        }
+
+        if (LIST_EMPTY(&env_sched_list[point])) {
+            point = 1 - point;
+        }
+    } while (e != NULL && e->env_status != ENV_RUNNABLE);
+
+    if (LIST_EMPTY(&env_sched_list[0]) && LIST_EMPTY(&env_sched_list[1])) {
+        //continue;
+        panic("Empty Schedule List");
+    }
+
+    assert(e != NULL);
+    assert(e->env_status == ENV_RUNNABLE);
+
+    count = e->env_pri - 1;
+    env_run(e);
 }
