c23ba9399025abe4cbc24905747ba8f6f74a8ef8
VOIDMalkuth <you@example.com>
2020-06-25 19:04:49 +0000

    Lab5-Challenge: allow backspace in shell

--- user/console.c
+++ user/console.c
@@ -54,7 +54,12 @@ cons_read(struct Fd *fd, void *vbuf, u_int n, u_int offset)
 
 	while ((c = syscall_cgetc()) == 0)
 		syscall_yield();
-
+    
+    if (c == 127) {
+        c = '\b';
+        writef("\b ");
+    }
+    
 	if (c!='\r') 
 		writef("%c",c);
 	else
