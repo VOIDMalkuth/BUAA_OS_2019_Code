5566742af742ecbac20c7d04b5d17f4eaea1eba1
VOIDMalkuth <you@example.com>
2020-06-26 14:05:31 +0000

    Lab5-Challenge: branch used for comparison

--- fs/serv.c
+++ fs/serv.c
@@ -309,6 +309,30 @@ umain(void)
 	writef("FS is running\n");
 
 	writef("FS can do I/O\n");
+    
+    writef("************Lab-5 Challenge Test!*************\n");
+    int r = 0, n = 0;
+
+    writef("NonFS Program Writing RTC\n");
+    n = 0x1234;
+    r = syscall_write_dev(&n, 0x15000010, 4);
+    writef("result is %d, data should be %d\n", r, n);
+    
+    n = 0x1;
+    writef("NonFS Program Reading RTC\n");
+    r = syscall_read_dev(&n, 0x15000010, 4);
+    writef("result is %d, data is %d\n", r, n);
+
+    writef("NonFS Program Writing fs\n");
+    n = 0x12345678;
+    r = syscall_write_dev(&n, 0x13004100, 4);
+    writef("result is %d, data should be %d\n", r, n);
+
+    n = 0x0;
+    writef("NonFS Program Reading fs\n");
+    r = syscall_read_dev(&n, 0x13004100, 4);
+    writef("result is %d, data is %d\n", r, n);
+    writef("************End of Lab-5 Challenge Test!*************\n");
 
 	serve_init();
 	fs_init();
